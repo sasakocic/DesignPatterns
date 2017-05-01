f87b6cb5b6948373e68c079f9193717b5ecac74f
Sasa Kocic <sasa.kocic@gmail.com>
2017-05-01 21:30:17 +0200

    fix typo

--- src/Factory/FactoryTest.java
+++ src/Factory/FactoryTest.java
@@ -4,7 +4,7 @@ public class FactoryTest {
     public static void main(String[] args) {
         Connection aConnection1 = ConnectionFactory.create();
         Connection aConnection2 = Connection.create();
-        System.out.println("Connection1 creted " + aConnection1.getClass());
-        System.out.println("Connection2 creted " + aConnection2.getClass());
+        System.out.println("Connection1 created " + aConnection1.getClass());
+        System.out.println("Connection2 created " + aConnection2.getClass());
     }
 }
